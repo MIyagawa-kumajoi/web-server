c8c16944dd3608b6d760254a789abb995015caa1
Hiro <miyagawa@kumajoi.com>
2021-06-19 13:45:48 +0900

    PORTを編集

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/api/v1/users', function (req, res) {
   });
 });
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => {
   console.log('I am running');
 });
